24b4db018368c807c6c36c3c4e2f3de84d834615
Sergio Pérez Montero <b381825@levante1.lvt.dkrz.de>
2025-07-21 11:16:48 +0200

    final version of GIS?

--- IceSheets/scripts/plot_ice_flow.jl
+++ IceSheets/scripts/plot_ice_flow.jl
@@ -3,31 +3,21 @@ Pkg.activate(".")
 using NCDatasets
 using CairoMakie
 using Interpolations
-using LaTeXStrings, MathTeXEngine
+using LaTeXStrings, MathTeXEngine, DSP
 
 include("tools.jl")
 
 z_map4 = cmap(:bukavu, [-4000, -3000, -2000, -1000, -500, -400, -300, -200, -100, -50, -10, 0.0, 10, 50, 100, 200, 300, 400, 500, 750, 1000, 1500, 2000], 1.0)
-H_map4 = cmap([:grey28, :lightsteelblue2, :snow], [10, 250, 500, 750, 1000, 1250, 1500, 1750, 2000, 2250, 2500, 2750, 3000, 3250, 3500, 3750, 4000], 1.0)
+H_map4 = cmap([:grey28, :lightsteelblue2, :snow], [10, 250, 500, 750, 1000, 1250, 1500, 1750, 2000, 2250, 2500, 2750, 3000, 3250, 3500, 3750, 4000, 4500], 1.0)
 zmap =  (colormap = z_map4, colorrange = (-4000, 2000))
-Hmap =  (colormap = H_map4, colorrange = (10, 4000), lowclip=:transparent, highclip=:white)
+Hmap =  (colormap = H_map4, colorrange = (10, 4500), lowclip=:transparent, highclip=:white)
 Hmap_aux = (colormap = H_map4, colorrange = (10, 4000), highclip=:white)
 
-# STOP
+animation_name = "/home/b/b381825/projects/material_for_presentations/IceSheets/flowmap_GRL-16KM_PD.mp4"
+path2data = "/home/b/b383208/models/yelmox_may25/output2/ismip/exp4/16KM/2/ctrl/yelmo2D.nc"
+create_yelmox_ice_flux_animation(path2data, animation_name, zmap, Hmap, Hmap_aux, n=350, maxsteps=2000, frmrt=5, time_range=0:1:100, thr=2250, density=0.5, grdsz=(48, 48), reset_traj=false, static_run=true, static_time=1)
 
-# animation_name = "/home/b/b381825/projects/material_for_presentations/IceSheets/flowmap_GRL-16KM_PD.mp4"
-# path2data = "/home/b/b383208/models/yelmox_may25/output2/ismip/exp4/16KM/2/ctrl/yelmo2D.nc"
-# create_yelmox_ice_flux_animation(path2data, animation_name, zmap, Hmap, Hmap_aux, n=100, maxsteps=500, frmrt=7, thr=2500, density=0.5, grdsz=(48, 48))
-
-# # STOP
-
-# animation_name = "/home/b/b381825/projects/material_for_presentations/IceSheets/flowmap_GRL-16KM_PD_slowvers.mp4"
-# path2data = "/home/b/b383208/models/yelmox_may25/output2/ismip/exp4/16KM/2/ctrl/yelmo2D.nc"
-# create_yelmox_ice_flux_animation(path2data, animation_name, zmap, Hmap, Hmap_aux, n=100, maxsteps=500, frmrt=4, thr=2500, density=0.5, grdsz=(48, 48), reset_traj=false)
-
-# STOP
-
-animation_name = "/home/b/b381825/projects/material_for_presentations/IceSheets/flowmap_ANT-32KM_PD.mp4"
-path2data = "/work/ba1442/sperezmont/yelmox_output/PMYY_MAG_Bipolar/exp02/bipolar_full_ramp01_both_1/south_yelmo2D.nc"
-create_yelmox_ice_flux_animation(path2data, animation_name, zmap, Hmap, Hmap_aux, frmrt=10, n=100, maxsteps=500, static_run=true, static_time=1)
+animation_name = "/home/b/b381825/projects/material_for_presentations/IceSheets/flowmap_ANT-16KM_PD.mp4"
+path2data = "/work/ba1442/sperezmont/yelmox_output/jsj_AIS_PD_for_ice_flow_animation/yelmo2D.nc"
+create_yelmox_ice_flux_animation(path2data, animation_name, zmap, Hmap, Hmap_aux, frmrt=5, time_range=0:1:500, n=150, maxsteps=2000, thr=3000, static_run=true, static_time=1, density=1.0, grdsz=(50, 50), reset_traj=false)
 
--- IceSheets/scripts/tools.jl
+++ IceSheets/scripts/tools.jl
@@ -3,7 +3,7 @@ Pkg.activate(".")
 using NCDatasets
 using CairoMakie
 using Interpolations
-using LaTeXStrings, MathTeXEngine
+using LaTeXStrings, MathTeXEngine, DSP, Statistics
 
 cmap(colors, levels, transp) = cgrad(colors,  round.((levels .- levels[1]) ./ abs(levels[1] - levels[end]), sigdigits=3), categorical=true, alpha=transp)
 
@@ -11,17 +11,20 @@ cmap(colors, levels, transp) = cgrad(colors,  round.((levels .- levels[1]) ./ ab
 This function calculates the trajectory followed by a particle for
 a given initial position `(x0, y0)`, a domain `(x,y)`, a velocity field `(u, v)` and a mask `H` during `times`
 """
-function calculate_trajectory(x0::Float32, y0::Float32, x::Array, y::Array, u::Array, v::Array, H::Array, times::Array; reset_traj::Bool=true)
+function calculate_trajectory(x0::Float32, y0::Float32, x::Array, y::Array, u::Array, v::Array, H::Array, z::Array, times::Array; reset_traj::Bool=true)
     traj, vels = [(x0, y0)], []
 
-    margins_mask = copy(H)
-    margins_mask[H .< 10.0] .= 0.0
-    margins_mask[H .> 10.0] .= 0.0
-    margins_mask[H .== 10.0] .= 1.0
+    # ice_margin = copy(H)
+    # ice_margin[H .< 1.0] .= NaN
+    # ice_margin[H > 1.0] .= NaN
+
+    # maxtravel_x, maxtravel_y = abs(x[1] - x[2]), abs(y[1] - y[2]) # assuming regular grid, get resolution
 
     u_itp = extrapolate(interpolate((x, y, times), u, Gridded(Linear())), 0.0)
     v_itp = extrapolate(interpolate((x, y, times), v, Gridded(Linear())), 0.0)
     H_itp = extrapolate(interpolate((x, y, times), H, Gridded(Linear())), 0.0)
+    z_itp = extrapolate(interpolate((x, y, times), z, Gridded(Linear())), 0.0)
+    # margin_itp = extrapolate(interpolate((x, y, times), ice_margin, Gridded(Linear())), 0.0)
 
     xt, yt = x0, y0
     push!(vels, sqrt(u_itp(x0, y0, 0)^2 + v_itp(x0, y0, 0)^2))
@@ -31,27 +34,37 @@ function calculate_trajectory(x0::Float32, y0::Float32, x::Array, y::Array, u::A
         tp1 = times[i+1] 
         dt = tp1 - t
 
-        # Check if there is ice at (x, y, t) and calculate u(x, y, t) and v(x, y, t)
+        # Calculate u(x, y, t) and v(x, y, t)
         Ht = H_itp(xt, yt, t)
-        if Ht < 10.0
-            ut, vt = 0.0, 0.0
+        if Ht < 10.0 # Check if there is ice at (x, y, t)
+            ut, vt = 0.0, 0.0 
         else
             ut = u_itp(xt, yt, t)
             vt = v_itp(xt, yt, t)
         end
-        push!(vels, sqrt(ut^2 + vt^2))
 
-        # Calculate new position (xtp1, ytp1, t+1)
         xtp1 = xt + ut*dt
         ytp1 = yt + vt*dt
 
-        # Check if there is ice
-        Htp1 = H_itp(xtp1, ytp1, tp1)
-        if Htp1 < 1.0    # the particle does not move
-            xtp1, ytp1 = xt, yt            
+        # Calculate new position (xtp1, ytp1, t+1) iteratively
+        # -- Check if new position is upstream
+        # -- Check if there is ice
+        ddt = dt
+        iter = 0
+        while (z_itp(xtp1, ytp1, tp1) > z_itp(xt, yt, t)) || (H_itp(xtp1, ytp1, tp1) < 1.0)
+            if iter > 50
+                break
+            end
+
+            xtp1 = xt + ut*ddt
+            ytp1 = yt + vt*ddt
+
+            ddt = 0.5*ddt
+            iter = iter + 1
         end
 
         # Store point (xtp1, ytp1)
+        push!(vels, sqrt(u_itp(xtp1, ytp1, t+1)^2 + v_itp(xtp1, ytp1, t+1)^2))
         push!(traj, (xtp1, ytp1))
         
         if (reset_traj) && (Htp1 < 1.0)    # Reset xt, yt to initial conditions
@@ -65,30 +78,95 @@ function calculate_trajectory(x0::Float32, y0::Float32, x::Array, y::Array, u::A
 end
 
 """
-This function computes the initial position `(x, y)` of `n` particles in `field` above `thr` 
+Finds the local maxima of the smoothed `field` above a threshold `thr`
+"""
+function find_local_maxima_above_thr(x, y, field, thr; window=5, smoothing_mat=1/9 .* [1 1 1; 1 1 1; 1 1 1])
+    nx, ny = size(field)
+    sfield = conv(smoothing_mat, field)
+    maximas, indexes = [], []
+    for i in window+1:nx-window, j in window+1:ny-window
+        if (mean(sfield[i-window:i+window, j-window:j+window]) >= thr)    # check if the mean of the area given by window is above thr 
+            for wx in 0:window, wy in 0:window                            # check height all around the point
+                if (sfield[i, j] >= sfield[i+wx, j+wy]) &&
+                     (sfield[i, j] >= sfield[i-wx, j-wy]) 
+
+                    if ((i, j) ∉ indexes)
+                        push!(maximas, (x[i], y[j]))
+                        push!(indexes, (i, j))  
+                    end 
+                end     
+            end
+        end
+    end
+    return maximas, indexes
+end
+
+# d = NCDataset("/home/b/b383208/models/yelmox_may25/output2/ismip/exp4/16KM/2/ctrl/yelmo2D.nc")
+# # d = NCDataset("/work/ba1442/sperezmont/yelmox_output/jsj_AIS_PD_for_ice_flow_animation/yelmo2D.nc")
+
+# x, y, z = d["xc"][:], d["yc"][:], d["z_srf"][:, :, 1]
+# mat = 1/9 .* [1 1 1; 1 1 1; 1 1 1]
+# z_smooth = conv(mat, z)
+# maxi, indexes = find_local_maxima_above_thr(d["xc"][:], d["yc"][:], d["z_srf"][:, :, 1], 2500, window=5)
+# fig = Figure()
+# ax = Axis(fig[1, 1])
+# ax2 = Axis(fig[1, 2])
+# heatmap!(ax, x, y, z)
+# heatmap!(ax2, x, y, z_smooth)
+# scatter!(ax2, maxi, color=:red)
+# fig
+
+
+"""
+This function computes the initial position `(x, y)` of `n` particles randomly started in local maximas chosed in `field` above `thr` 
 """
 function calculate_initial_positions(n, x, y, field, thr)
     if n == 1
         indexes = [argmax(field)]
     else
-        indexes = findall( x -> x>=thr, field)
-        indexes = indexes[sortperm(field[indexes], rev=true)]  # sort the indexes by their value
-        indexes = indexes[Int.(floor.(range(1, stop=length(indexes), length=n)))]
+        maxima, indexes = find_local_maxima_above_thr(x, y, field, thr)#findall( z -> z>=thr, field)
+
+        # if length(indexes) > n  # chose randomly n values
+        #     new_indexes, positions = [], collect(1:length(indexes))
+        #     while length(new_indexes) < n
+        #         rand_index = rand(positions)
+
+        #         if rand_index ∉ new_indexes
+        #             push!(new_indexes, rand_index)
+        #         end
+        #     end
+        #     indexes = indexes[new_indexes]
+        # end
+
     end
     return [(x[indexes[i][1]], y[indexes[i][2]]) for i in eachindex(indexes)]
+end
+
+function create_list_of_colors(data, colormap, levels)
+    # data = [series, time]
+    normalized_levels = levels ./ maximum(levels)
+
+    colormap2 = cgrad(colormap, normalized_levels, categorical=true)
+    vector_of_colors = collect(colormap2)
+
+    colors = Array{typeof(vector_of_colors[1])}(undef, size(data))
+    for i in eachindex(data[:, 1]), j in eachindex(data[1, :])
+        colors[i, j] = vector_of_colors[findmin(abs.(data[i, j] .- levels[2:end]))[2]] # levesl[1] is 0
+    end
+    return colors, colormap2
 end
 
 """
 This function creates `plotname` animation from a YelmoX experiment located in `path2run`
 """
-function create_yelmox_ice_flux_animation(path2run::String, plotname::String, bat_cmap_kwargs, ice_cmap_kwargs, ice_cmap_aux_kwargs; frmrt=5, n=20, thr=2500, figsize=(900, 800), fs=22, lw=1, rw=0.45, grdsz=(48, 48), density=1.0, maxsteps=150, stepsize=2, reset_traj=true, static_run=false, static_time=1)
+function create_yelmox_ice_flux_animation(path2run::String, plotname::String, bat_cmap_kwargs, ice_cmap_kwargs, ice_cmap_aux_kwargs; frmrt=5, n=20, thr=2500, figsize=(900, 800), fs=22, lw=1, rw=0.45, grdsz=(48, 48), density=1.0, maxsteps=150, stepsize=2, reset_traj=true, static_run=true, static_time=1, time_range=0:1:1000)
     # Load data
-    data = NCDataset(path2data)
+    data = NCDataset(path2run)
     x, y = view(data["xc"])[:], view(data["yc"])[:]
 
     # Load some variables
     if static_run
-        time_2D = collect(Float32, 0:1:100)  # fixed
+        time_2D = collect(Float32, time_range)  # kyr
         times2use = static_time
         H_ice = Array{Float32}(undef, length(x), length(y), length(time_2D)) 
         z_bed = Array{Float32}(undef, length(x), length(y), length(time_2D)) 
@@ -112,17 +190,17 @@ function create_yelmox_ice_flux_animation(path2run::String, plotname::String, ba
     n_2D = length(time_2D)
     z_srf = H_ice .+ z_bed
 
-    uxy_s_map4 = cgrad([:white, :darkred], range(0, stop = 1, length = 11), categorical = true)
-    lin_umap = (colormap=uxy_s_map4, colorrange=(1f-8, 1000), colorscale=Makie.pseudolog10,
-                lowclip=:transparent, highclip=uxy_s_map4[end])
-
     # Calculate r(x,y,t) of n particles
+    # z_srf_mod = copy(z_srf[:, :, static_time])
+    # z_srf_mod[H_ice[:, :, static_time] .<= 1.0] .= -Inf
     rxy = calculate_initial_positions(n, x, y, z_srf[:, :, static_time], thr)
     particle_tracks = Array{Tuple{Float32, Float32}}(undef, length(rxy), length(time_2D)) 
     particle_velocities = Array{Float32}(undef, length(rxy), length(time_2D)) 
     for i in eachindex(rxy)
-        particle_tracks[i, :], particle_velocities[i, :] = calculate_trajectory(rxy[i][1], rxy[i][2], x, y, ux_s[:,:,:], uy_s[:, :, :], H_ice[:, :, :], time_2D[:], reset_traj=reset_traj)
+        particle_tracks[i, :], particle_velocities[i, :] = calculate_trajectory(rxy[i][1], rxy[i][2], x, y, ux_s[:,:,:], uy_s[:, :, :], H_ice[:, :, :], z_srf[:, :, :], time_2D[:], reset_traj=reset_traj)
     end
+
+    velocity_colors, velocity_cmap = create_list_of_colors(Makie.pseudolog10.(particle_velocities), :thermal, range(0, stop=Makie.pseudolog10(1500), length=11))
 
     # Define the observables to let Julia recognize the time dimension
     if static_run
@@ -134,14 +212,16 @@ function create_yelmox_ice_flux_animation(path2run::String, plotname::String, ba
     H_ice_obs = @lift(H_ice[:, :, $k])
     z_bed_obs = @lift(z_bed[:, :, $k])
     z_srf_obs = @lift(z_srf[:, :, $k])
-    t_obs = @lift("t = $(round(time_2D[$k])) yr")
+    t_obs = @lift("t = $(round(time_2D[$k])) kyr")
     particle_track_obs = @lift(particle_tracks[:, $k])
     particle_velocities_obs = @lift(particle_velocities[:, $k])
+    velocity_colors_obs = @lift(velocity_colors[:, $k])
+
 
     ux_itp = linear_interpolation((x, y, time_2D[:]), ux_s[:, :, :])    # velocity field interpolators
     uy_itp = linear_interpolation((x, y, time_2D[:]), uy_s[:, :, :])
 
-    f(xy, t::Float32) = Point2f(ux_itp(xy[1], xy[2], t), uy_itp(xy[1], xy[2], t))   # function to plot the streamlines
+    f(xy, t::Float32) = Point2f(Makie.pseudolog10(ux_itp(xy[1], xy[2], t)), Makie.pseudolog10(uy_itp(xy[1], xy[2], t)))   # function to plot the streamlines
     sf = Observable(Base.Fix2(f, 0.0f0))
 
     # Set up animation
@@ -157,33 +237,33 @@ function create_yelmox_ice_flux_animation(path2run::String, plotname::String, ba
     heatmap!(ax, x, y, H_ice_obs; ice_cmap_kwargs...)
     H_hm = heatmap!(ax, x, y, H_ice[:, :, 1] .* NaN; ice_cmap_aux_kwargs...)
     contour!(ax, x, y, z_srf_obs, color=:grey30, levels=0:500:4000, linestyle=:dashdot)
-    streamplot!(ax, sf, x[1]..x[end], y[1]..y[end], gridsize=grdsz, linewidth=lw, arrow_size=0.0, maxsteps=maxsteps, stepsize=stepsize, density=density, colormap=[:black])
+    strm = streamplot!(ax, sf, x[1]..x[end], y[1]..y[end], gridsize=grdsz, linewidth=lw, arrow_size=0.0, maxsteps=maxsteps, stepsize=stepsize, density=density, colormap=velocity_cmap, lowclip=:transparent, highclip=velocity_cmap[end])
     
-    uxy_s_map4 = cgrad(:thermal, range(0, stop = 1, length = 11), categorical=true)
-    scatter!(ax, particle_track_obs, color=particle_velocities_obs, colormap=uxy_s_map4)
-    lin_umap=(colormap = :thermal, colorrange=(1f-8, 1000), highclip=uxy_s_map4[end])
+    scatter!(ax, particle_track_obs, color=velocity_colors_obs) #, color=range(0, stop=1000, length=length(rxy)), colormap=uxy_s_map4)
 
     Colorbar(fig[1, 2], z_hm, vertical = true, flipaxis = true, height = Relative(rw),
         valign = :top, label = "Bed elevation (m)")
     Colorbar(fig[1, 2], H_hm, vertical = true, flipaxis = true, height = Relative(rw),
         valign = :bottom, label = "Ice thickness (m)")
 
-    Colorbar(fig[1, 3], vertical = true, flipaxis = true, height = Relative(rw),
-        valign = :bottom, label = "Surface velocity (m/yr)", ticks = 0:200:1000; lin_umap...)
+    ticks = [0, 1, 10, 100, 1000]
+    Colorbar(fig[1, 3], strm, vertical = true, flipaxis = true, height = Relative(rw),
+        valign = :bottom, label = "Surface velocity (m/yr)", ticks = (Makie.pseudolog10.(ticks), string.(ticks)))
 
-    colgap!(fig.layout, 0.0)
+    colgap!(fig.layout, 1, 10.0)
+    colgap!(fig.layout, 2, 0.0)
     
     save("$(plotname).png", fig)
 
     # Create animation
     record(fig, "$(plotname)", 1:n_2D, framerate = frmrt) do i
-        # if static_run
-        #     k[] = static_time
-        #     sf[] = Base.Fix2(f, time_2D[i])
-        # else
         k[] = i
         sf[] = Base.Fix2(f, time_2D[i])
+
+        # for p in eachindex(particle_track_obs.val[:])
+        #     scatter!(ax, particle_track_obs.val[p], color=velocity_colors_obs.val[p]) #, color=range(0, stop=1000, length=length(rxy)), colormap=uxy_s_map4)
         # end
+
     end
     rm("$(plotname).png")
     return nothing
